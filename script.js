bc9049de78d5b0e618bf537f7e18b0c540820409
Amanda Ramirez <78186370+amandadeogr@users.noreply.github.com>
2021-04-18 22:33:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,10 @@ function iniciar() {
       `${dados.getDate()}/${dados.getMonth()+1}/${dados.getFullYear()}`
     ]
 
-  //realiza a captura da temperatura local
-  //let clima = 
+  /*
+  realizará a captura da temperatura local
+  let clima = 
+  */
 
   //exiibição das informações
     let exibirHora = document.getElementById('hAtual');
@@ -24,7 +26,7 @@ function iniciar() {
     let exibirData = document.getElementById('dAtual')
     exibirData.innerHTML = `${data}`
 
-   //com operador ternário
+   //mudança de background com operador ternário
     dados.getHours() > 6 && dados.getHours() < 12 ? document.body.style.backgroundImage = "url(https://i.pinimg.com/originals/48/11/8a/48118af7ff6b767a4aab266527d81b6e.jpg)": dados.getHours() > 12 && dados.getHours() < 18 ? document.body.style.backgroundImage = "url(https://www.xmple.com/wallpaper/brown-orange-gradient-linear-1920x1080-c2-800000-ff8c00-a-90-f-14.svg)" : document.body.style.backgroundImage = "url(https://wallpapercave.com/wp/HeYmP7w.png)";
 
 
